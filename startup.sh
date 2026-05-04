4d4ecdb63c7130fc70aba9e22afb706a77c39966
Niranjan Gc <niranjangc.me@gmail.com>
2026-05-04 23:27:15 +0200

    live

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i 's|/home/site/wwwroot/index.php|/home/site/wwwroot|g' /etc/nginx/sites-available/default
-sed -i 's|index  index.php index.html index.htm hostingstart.html;|index  index.php index.html;|g' /etc/nginx/sites-available/default
-service nginx reload
